3bf9b0baee844c04f0e6b53a0985cacb3e97eb9b
REACTking5566 <ssadd502224@gmail.com>
2019-07-10 23:59:36 +0800

    ggttyy

--- src/js/component/test.jsx
+++ src/js/component/test.jsx
@@ -2,7 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export let Test = () => {
-    return <div>jkjkjerwerwkjk</div>
+    return <header>
+        <div className="header-top-content"></div>
+        <div className="header-middle-content">
+            <div className="content-inner-y">
+                <div className="container">
+                    <div className="row">
+                        <div className="heder-logo">
+                            <h1 className="header-logo">
+                                <a href="">
+                                    <img src="" alt="" />
+                                </a>
+                            </h1>
+                        </div>
+                        <div className="nav">
+                            <nav>
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div className="header-banner">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </header>
 }
 
 
