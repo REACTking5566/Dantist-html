1fb4d355776c1f60d58cd57cd78b4520080bbb22
REACTking5566 <ssadd502224@gmail.com>
2019-07-14 18:35:40 +0800

    updateHtml

--- src/js/component/test.jsx
+++ src/js/component/test.jsx
@@ -19,16 +19,11 @@ export let Test = () => {
                         <div className="nav">
                             <nav>
                                 <ul>
-                                    <li>ギフト・贈り物</li>
-                                    <li>ギフト・贈り物</li>
-                                    <li>ギフト・贈り物</li>
-                                    <li>ギフト・贈り物</li>
-                                    <li>ギフト・贈り物</li>
-                                    <li>ギ8989フト・贈り物</li>
+                                    <li>053-416-1188</li>
                                 </ul>
                             </nav>
                         </div>
-                        <div className="header-banner">
+                        <div className="header-banner" style={{ display: "none" }}>
                             <ul>
                                 <li>
                                     <div className="form-search">
@@ -48,12 +43,104 @@ export let Test = () => {
                                 </li>
                             </ul>
                         </div>
+
                     </div>
                 </div>
 
             </div>
         </div>
+        <div className="main">
+            <div className="bg">
+                <div className="content-inner-y">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-10">
+                                <div className="main-img">
 
+                                    <div className="main-img-slide">
+                                        <ul>
+                                            <li>
+                                                <img src="https://www.haru-shika.jp/wp/wp-content/themes/haru/images/main-visual03.jpg" alt="" />
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="main-img-logo">
+                                        <a href="">
+                                            <img src="https://www.haru-shika.jp/wp/wp-content/themes/haru/common/images/logo_pc.svg" alt="" />
+                                        </a>
+                                    </div>
+                                    <div className="main-copy">
+                                        <a href="">
+                                            <img src="https://www.haru-shika.jp/wp/wp-content/themes/haru/images/main-copy.png" alt="" />
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="pc">
+
+                                    <nav>
+                                        <ul>
+                                            <li className="font-vt fw700">自費治療について</li>
+                                            <li className="font-vt fw700">自費治療について</li>
+                                            <li className="font-vt fw700">自費治療について</li>
+                                            <li className="font-vt fw700">自費治療について</li>
+                                            <li className="font-vt fw700">自費治療について</li>
+                                            <li className="font-vt fw700">自費治療について</li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                            <div className="col-md-2">
+                                <div className="nav-sub-header text-center">
+                                    <a href="" className="btn-wrap">
+                                        <span>recureit</span>
+                                    </a>
+                                    <p>
+                                        浜松市中区小豆餅の歯科医院
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <section className="content">
+            <div className="bg">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-2">
+                            <div className="news-sec-ttl">
+                                <h2>
+                                    <span>お知らせ</span>
+                                    <span>お知らせ</span>
+                                </h2>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="news-sec-time">
+                                <ul>
+                                    <li className='mb20'>
+                                        <span>2018.7.20</span>
+                                        <a href="">8月11日（土）～16日（木）まで休診とさせていただきます。  </a>
+                                    </li>
+                                    <div className="bottom-white-line mb20"></div>
+                                    <li className='mb20'>
+                                        <span>2018.7.20</span>
+                                        <a href="">8月11日（土）～16日（木）まで休診とさせていただきます。  </a>
+                                    </li>
+                                    <div className="bottom-white-line mb20"></div>
+                                    <li className='mb20'>
+                                        <span>2018.7.20</span>
+                                        <a href="">8月11日（土）～16日（木）まで休診とさせていただきます。  </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </header>
 }
 
